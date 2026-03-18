79bcde23bdc3cd20f193b96eeb45774198ea12fc
josemario234 <AL02963295@tecmilenio.mx>
2026-03-17 22:21:43 -0700

    Finish editing the php file "show.blade.php" and changing other details

--- grades/resources/views/grades/show.blade.php
+++ grades/resources/views/grades/show.blade.php
@@ -4,11 +4,21 @@
         <meta charset="UTF-8">
         <meta name="Viewport" content="width=device-width, initial-scale=1.0">
         <title>View Activity</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div class="container">
-            <h1></h1>
+            <h1>Evaluable Activity Details</h1>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Subject: {{ $grade->subject }}</h5>
+                <h5 class="card-text">Activity: {{ $grade->activity }}</h5>
+                <h5 class="card-text">Date: {{ $grade->date }}</h5>
+                <h5 class="card-text">Grade: {{ $grade->grade }}</h5>
+                </div>
+            </div>
+            <a href="{{ route('grades.index') }}" class="secondary">Back</a>
+            <a href="{{ route('grades.edit', $grade->id) }}" class="warning">Edit</a>
         </div>
     </body>
 </html>
